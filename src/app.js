0d6c18bebd9d3e2ed5e5a6c2907d560ab048df38
Bishal Samanta <web.dev.bishalsamanta@gmail.com>
2022-02-23 13:44:53 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -38,12 +38,13 @@ app.use("/venues" , venueDetailsController);
 //Use Controllers with route end
 //Main Code Ends
 
+let port = process.env.PORT || 1232
 
 //Start Server
-app.listen(1232, async ()=>{
+app.listen(port, async ()=>{
     try{
         await connect();
-        console.log("Listening on port 1232");
+        console.log(`Listening on port ${port}`);
     }
     catch(e){
         console.error("Error is" , e.message);
