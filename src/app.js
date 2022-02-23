b83af31678787c07ecf105e9db00427592269d78
Bishal Samanta <web.dev.bishalsamanta@gmail.com>
2022-02-23 20:23:22 +0530

    testing for cors

--- src/app.js
+++ src/app.js
@@ -7,6 +7,16 @@ app.use(express.json());
 const path = require("path");
 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
+
+
 
 
 //Connect with database start
